ed9b71046816d7afb727182f9f41b971967dde33
St-Jousef <Joseph_nkwocha>
2023-07-18 10:56:52 +0100

    added a signup/login form

--- CLIENT/src/Components/Login/Login.jsx
+++ CLIENT/src/Components/Login/Login.jsx
@@ -1,31 +1,115 @@
-import React from 'react'
-import Navigation from '../Navigation/Navigation'
-import { useContext } from 'react'
-import { myUserContext } from '../../Utilities/UserContext'
+import Navigation from "../Navigation/Navigation";
+import "./Login.css";
+import { React, useContext, useState } from "react";
+import Button from "../../Utilities/Button";
+import { Form, Modal } from "react-bootstrap";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { myUserContext } from "../../Utilities/UserContext";
 
 const Login = () => {
+  const { user, handleLoginPage, handleSignUpPage, homePageRedirect } =
+    useContext(myUserContext);
 
-const {user, handleLoginPage, handleSignUpPage, homePageRedirect} = useContext(myUserContext)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [rememberMe, setRememberMe] = useState(false);
 
-return (
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-<>
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-    <header>
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-        <Navigation user = {user} handleLoginPage = {handleLoginPage} handleSignUpPage={handleSignUpPage} homePageRedirect={homePageRedirect}></Navigation>
+  const handleRememberMe = () => {
+    setRememberMe(!rememberMe);
+  };
 
-    </header>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add my login logic here
+  };
+  return (
+    <>
+      <header>
+        <Navigation
+          user={user}
+          handleLoginPage={handleLoginPage}
+          handleSignUpPage={handleSignUpPage}
+          homePageRedirect={homePageRedirect}
+        ></Navigation>
+      </header>
 
-    <section>
+      <section className="flex-container">
+        <div className="login-img">
+          <img src="../src/assets/smartcity-remo.png" alt="login" />
+        </div>
 
-        <h1> Login Page </h1>
-        
-    </section>
-</>
+        {/* <h1> Login Page </h1> */}
+        <Modal.Dialog>
+          <Modal.Header>
+            <Modal.Title>Login Page</Modal.Title>
+          </Modal.Header>
+          <span>Welcome back! Please enter your details.</span>
+          <br />
+          <Modal.Body>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+              </Form.Group>
 
-)
+              <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <div className="password-input-container">
+                  <Form.Control
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Enter password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                  />
+                  <div className="password-toggle" onClick={handleShowPassword}>
+                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                  </div>
+                </div>
+              </Form.Group>
 
-}
+              <Form.Group controlId="rememberMe">
+                <Form.Check
+                  type="checkbox"
+                  label="Remember me"
+                  checked={rememberMe}
+                  onChange={handleRememberMe}
+                />
+              </Form.Group>
+              <br />
 
-export default Login
+              <Button type="submit" variant="primary">
+                Login
+              </Button>
+            </Form>
+          </Modal.Body>
+          <br />
+          <br />
+          <div className="hr-strike">
+            <span>Or</span>
+          </div>
+        </Modal.Dialog>
+      </section>
+      <section></section>
+    </>
+  );
+};
+
+export default Login;
--- CLIENT/src/Components/SignUp/Signup.jsx
+++ CLIENT/src/Components/SignUp/Signup.jsx
@@ -1,31 +1,138 @@
-import React from 'react'
-import Navigation from '../Navigation/Navigation'
-import { useContext } from 'react'
-import { myUserContext } from '../../Utilities/UserContext'
-
-
+import Navigation from "../Navigation/Navigation";
+import "./Signup.css";
+import { React, useContext, useState } from "react";
+import Button from "../../Utilities/Button";
+import { Form, Modal } from "react-bootstrap";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { myUserContext } from "../../Utilities/UserContext";
 
 const Signup = () => {
+  const { user, handleLoginPage, handleSignUpPage, homePageRedirect } =
+    useContext(myUserContext);
 
-const {user, handleLoginPage, handleSignUpPage, homePageRedirect} = useContext(myUserContext)
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [rememberMe, setRememberMe] = useState(false);
 
-return (
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
 
-<>
-    <header>
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-    <Navigation user = {user} handleLoginPage = {handleLoginPage} handleSignUpPage={handleSignUpPage} homePageRedirect={homePageRedirect}></Navigation>
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-    </header>
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-    <section>
+  const handleRememberMe = () => {
+    setRememberMe(!rememberMe);
+  };
 
-        <h1> sign up page </h1>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add my signup logic here
+  };
 
-    </section>
-</>
+  return (
+    <>
+      <header>
+        <Navigation
+          user={user}
+          handleLoginPage={handleLoginPage}
+          handleSignUpPage={handleSignUpPage}
+          homePageRedirect={homePageRedirect}
+        ></Navigation>
+      </header>
 
-)
-}
+      <section className="flex-container">
+        <div className="login-img">
+          <img src="../src/assets/smartcity-remo.png" alt="login" />
+        </div>
 
-export default Signup
+        <Modal.Dialog>
+          <Modal.Header>
+            <Modal.Title>Sign Up</Modal.Title>
+          </Modal.Header>
+          <span>To sign up, fill in your personal details below</span>
+          <br />
+          <Modal.Body>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="name">
+                <Form.Label>
+                  Name
+                  <span style={{ color: "red" }}> *</span>
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter name"
+                  value={name}
+                  onChange={handleNameChange}
+                />
+              </Form.Group>
+
+              <Form.Group controlId="email">
+                <Form.Label>
+                  Email
+                  <span style={{ color: "red" }}> *</span>
+                </Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+              </Form.Group>
+
+              <Form.Group controlId="password">
+                <Form.Label>
+                  Password
+                  <span style={{ color: "red" }}> *</span>
+                </Form.Label>
+                <div className="password-input-container">
+                  <Form.Control
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Enter password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                  />
+                  <div className="password-toggle" onClick={handleShowPassword}>
+                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                  </div>
+                </div>
+              </Form.Group>
+
+              <Form.Group controlId="rememberMe">
+                <Form.Check
+                  type="checkbox"
+                  label="Remember me"
+                  checked={rememberMe}
+                  onChange={handleRememberMe}
+                />
+              </Form.Group>
+              <br />
+
+              <Button type="submit" variant="primary">
+                Sign In
+              </Button>
+            </Form>
+          </Modal.Body>
+          <br />
+          <div className="hr-strike">
+            <span>Or</span>
+          </div>
+        </Modal.Dialog>
+      </section>
+      <section></section>
+    </>
+  );
+};
+
+export default Signup;
